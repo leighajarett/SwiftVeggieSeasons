42645eef4a17f61c5d26784f7da06b4d763cec52
Leigha Jarett <leigha.jarett@gmail.com>
2023-05-19 13:48:04 -0400

    Initial Commit

---
+++ SwiftVeggieSeasons/SwiftVeggieSeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVeggieSeasonsApp.swift
+//  SwiftVeggieSeasons
+//
+//  Created by Leigha Jarett on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVeggieSeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftVeggieSeasonsUITests/SwiftVeggieSeasonsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftVeggieSeasonsUITestsLaunchTests.swift
+//  SwiftVeggieSeasonsUITests
+//
+//  Created by Leigha Jarett on 5/19/23.
+//
+
+import XCTest
+
+final class SwiftVeggieSeasonsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
